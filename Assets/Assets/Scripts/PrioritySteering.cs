f6bbc675d4d2290c764573c7582dd15e72f26121
SamEBaker <esparzs1@my.erau.edu>
2025-02-06 00:05:30 -0700

    asset

---
+++ Assets/Assets/Scripts/PrioritySteering.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PrioritySteering
+{
+    public float epsilon = 0.1f;
+    public BlendSteering[] groups;
+
+    public SteeringOutput getSteering()
+    {
+        SteeringOutput steering = new SteeringOutput();
+        foreach(BlendSteering group in groups)
+        {
+            steering = group.getSteering();
+            if(steering.linear.magnitude > epsilon || Mathf.Abs(steering.angular) > epsilon)
+            {
+                return steering;
+            }
+        }
+        return steering;
+    }
+}
